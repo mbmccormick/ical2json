affd224b6c3c7f0efcc0f888e0a38e4b461ecc28
Matt McCormick <matt@mbmccormick.com>
2014-05-26 01:28:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, request
 from icalendar import Calendar
+from PyRSS2Gen import PyRSS2Gen
 from urllib import urlopen
 
 app = Flask(__name__)
@@ -37,10 +38,37 @@ def convert_from_url(url):
 
         data[cal.name][component.name].append(comp_obj)
 
-    resp = jsonify(data)
-    if 'callback' in request.args:
-        resp.data = "%s(%s);" % (request.args['callback'], resp.data)
-    return resp
+    resp = PyRSS2Gen.RSS2(
+        title = "Andrew's PyRSSGen feed",
+        link = "http://www.dalk2escientific.com/Python/PyRSS2Gen.html",
+        description = "The latest news about PyRSS2Gen, a "
+                  "Python library for generating RSS2 feeds",
+
+        lastBuildDate = datetime.datetime.now(),
+
+        items = [
+            PyRSS2Gen.RSSItem(
+                title = "PyRSS2Gen-0.0 released",
+                link = "http://www.dalkescientific.com/news/030906-PyRSS2Gen.html",
+                description = "Dalke Scientific today announced PyRSS2Gen-0.0, "
+                       "a library for generating RSS feeds for Python.  ",
+                guid = PyRSS2Gen.Guid("http://www.dalkescientific.com/news/"
+                          "030906-PyRSS2Gen.html"),
+                pubDate = datetime.datetime(2003, 9, 6, 21, 31)),
+            PyRSS2Gen.RSSItem(
+                title = "Thoughts on RSS feeds for bioinformatics",
+                link = "http://www.dalkescientific.com/writings/diary/"
+                "archive/2003/09/06/RSS.html",
+                description = "One of the reasons I wrote PyRSS2Gen was to "
+                       "experiment with RSS for data collection in "
+                       "bioinformatics.  Last year I came across...",
+                guid = PyRSS2Gen.Guid("http://www.dalkescientific.com/writings/"
+                               "diary/archive/2003/09/06/RSS.html"),
+                pubDate = datetime.datetime(2003, 9, 6, 21, 49)),
+            ]
+        )
+
+    return resp.to_xml()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
