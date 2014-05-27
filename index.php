6dcf19c937e6da1bf199df38fb8d9f2bd6a1c955
Matt McCormick <mbmccormick@outlook.com>
2014-05-26 22:40:24 -0700

    Formatting as RSS.

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 	require "class.iCalReader.php";
 
 	$ical = new ical($_GET["path"]);
-	print_r($ical->events());
+
+	print("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+	print("<rss version=\"2.0\">\n");
+	print("<channel>\n");
+
+	print("<title>ical2rss feed</title>\n");
+	print("<description>An ical feed converted to RSS.</description>\n");
+	print("<link>" . $_GET["path"] . "</link>\n");
+	print("<lastBuildDate>" . date(DATE_RSS) . "</lastBuildDate>\n");
+	print("<pubDate>" . date(DATE_RSS) . "</pubDate>\n");
+	print("<ttl>1800</ttl>\n");
+
+	foreach ($ical->events() as $event)
+	{
+		print("<item>\n");
+		print("<title>" . $event["SUMMARY"] . "</title>\n");
+		print("<description>" . $event["DESCRIPTION"] . "</description>\n");
+		print("<link>" . $event["UID"] . "</link>\n");
+		print("<guid>" . $event["UID"] . "</guid>\n");
+		print("<pubDate>" . $event["DTSTART"] . "</pubDate>\n");
+		print("</item>\n");
+	}
+
+	print("</channel>\n");
+	print("</rss>\n");
 
 ?>
